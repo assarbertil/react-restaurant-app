a1de12b5495f5ab7c632a0434ad7a3b9cbefcf35
Assar <33634310+assarbertil@users.noreply.github.com>
2022-04-04 11:06:54 +0200

    Stitches basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Text } from "./components/Text";
+
 function App() {
   return (
     <div>
-      <h1>asdasd</h1>
+      <Text>asdasdsd</Text>
+      <Text as="h1" type="title1">
+        asdasdsd
+      </Text>
     </div>
   );
 }
