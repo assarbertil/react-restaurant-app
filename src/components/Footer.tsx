c9b180017afb08bf41af17d586333a908330b51f
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-04-04 15:39:31 +0200

    testing with typo in admin link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,6 @@ import { styled } from '../stitches.config'
 import { Link } from 'react-router-dom'
 import { Text } from './primitives/Text'
 
-
 const FooterElement = styled('header', {
   display: 'flex',
   flexDirection: 'row',
@@ -13,31 +12,21 @@ const FooterElement = styled('header', {
 })
 
 export const Footer = () => {
-
   return (
     <FooterElement>
       <Link to="/kontakt">
-        <Text
-          type="large"
-          css={{ color: '$secondary' }}
-        >
+        <Text type="large" css={{ color: '$secondary' }}>
           Kontakt
         </Text>
       </Link>
       <Link to="/boka">
-        <Text
-          type="large"
-          css={{ color: '$secondary', marginLeft: '2rem' }}
-        >
+        <Text type="large" css={{ color: '$secondary', marginLeft: '2rem' }}>
           Boka bord
         </Text>
       </Link>
       <Link to="/admin">
-        <Text
-          type="large"
-          css={{ color: '$secondary', marginLeft: '2rem' }}
-        >
-          Admin
+        <Text type="large" css={{ color: '$secondary', marginLeft: '2rem' }}>
+          Adminnnnn
         </Text>
       </Link>
     </FooterElement>
