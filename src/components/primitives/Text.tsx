99bc9747e4e1dae1207f199bec4db7114b3bf67f
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-04-04 14:10:26 +0200

    install dependencies & format files & add .env

--- src/components/primitives/Text.tsx
+++ src/components/primitives/Text.tsx
@@ -1,18 +1,18 @@
-import { styled } from "../../stitches.config";
+import { styled } from '../../stitches.config'
 
-export const Text = styled("p", {
-  lineHeight: "$base",
+export const Text = styled('p', {
+  lineHeight: '$base',
 
   variants: {
     type: {
-      title1: { fontSize: "$title1" },
-      title2: { fontSize: "$title2" },
-      title3: { fontSize: "$title3" },
-      title4: { fontSize: "$title4" },
+      title1: { fontSize: '$title1' },
+      title2: { fontSize: '$title2' },
+      title3: { fontSize: '$title3' },
+      title4: { fontSize: '$title4' },
 
-      large: { fontSize: "$large" },
-      regular: { fontSize: "$regular" },
-      small: { fontSize: "$small" },
-    },
-  },
-});
+      large: { fontSize: '$large' },
+      regular: { fontSize: '$regular' },
+      small: { fontSize: '$small' }
+    }
+  }
+})
--- src/stitches.config.js
+++ src/stitches.config.js
@@ -1,41 +1,41 @@
-import { createStitches } from "@stitches/react";
+import { createStitches } from '@stitches/react'
 
 export const { styled } = createStitches({
   theme: {
     colors: {
-      primary: "#B00000",
-      secondary: "#EFA438",
+      primary: '#B00000',
+      secondary: '#EFA438',
 
-      background: "#111111",
-      dark: "#666666",
-      mid: "#AAAAAA",
-      light: "#CCCCCC",
-      foreground: "#EEEEEE",
+      background: '#111111',
+      dark: '#666666',
+      mid: '#AAAAAA',
+      light: '#CCCCCC',
+      foreground: '#EEEEEE'
     },
 
     fonts: {
-      tiles: "Oswald, sans-serif",
-      body: "Roboto, sans-serif",
+      tiles: 'Oswald, sans-serif',
+      body: 'Roboto, sans-serif'
     },
 
     lineHeights: {
-      base: 1.5,
+      base: 1.5
     },
 
     fontSizes: {
-      title1: "2.625rem",
-      title2: "2rem",
-      title3: "1.5rem",
-      title4: "1.125rem",
+      title1: '2.625rem',
+      title2: '2rem',
+      title3: '1.5rem',
+      title4: '1.125rem',
 
-      large: "1.125rem",
-      regular: "1rem",
-      small: "0.875rem",
+      large: '1.125rem',
+      regular: '1rem',
+      small: '0.875rem'
     },
 
     space: {
       4: 4,
-      8: 8,
-    },
-  },
-});
+      8: 8
+    }
+  }
+})
