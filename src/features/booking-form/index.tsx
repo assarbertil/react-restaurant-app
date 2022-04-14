5e00d64a9c2615c169e65d936e2be73086e5f1fa
Frida Planting <94045606+fplanting@users.noreply.github.com>
2022-04-14 15:41:59 +0200

    Dev (#44)
    * testing with typo in admin link
    
    * Trying to fix diff in branches
    
    * add background img, finish home page design, import font
    
    * added routes and booking-features
    
    * add interfaces & test booking function
    
    * made some booking form components
    
    * wip: dateForm
    
    * Date picker separated in to own file
    
    * Booking works
    
    * tiny tweaks
    
    * some fixes on datepicker and form validation
    
    * booking form and datepicker improvements
    
    * lots of small improvements
    
    * fixed language translator
    
    * added create functionality to admin page
    
    * fixed some styling
    
    * styling and animations
    
    * made it possible to book multiple tables at once
    
    * commented code and added more transitions
    
    * added swedish translation to validation
    
    * small change to swedish translation
    
    * 404 page implemented
    
    * fixed translation and notfoundpage
    
    Co-authored-by: Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
    Co-authored-by: Assar <33634310+assarbertil@users.noreply.github.com>

--- src/features/booking-form/index.tsx
+++ src/features/booking-form/index.tsx
@@ -9,7 +9,7 @@ import { FieldGroup } from './FieldGroup'
 import { styled } from 'stitches.config'
 import { CustomDatePicker } from './DatePicker'
 import { Formik, Form } from 'formik'
-import { BookingSchema } from './BookingSchema'
+//import { BookingSchema } from './BookingSchema'
 import { IFormValues } from '@/interfaces/FormValues'
 import { postBooking, sleep } from 'lib'
 import { useState } from 'react'
@@ -18,6 +18,7 @@ import { sv, enGB } from 'date-fns/locale'
 import { registerLocale } from 'react-datepicker'
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import * as Yup from 'yup'
 
 // Makes locales usable
 registerLocale('sv', sv)
@@ -26,10 +27,41 @@ registerLocale('en', enGB)
 export const BookingForm = () => {
   const [submitted, setSubmitted] = useState(false)
   const { t, i18n } = useTranslation()
+
+  const BookingSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, t('validation:firstNameMin'))
+      .max(20, t('validation:firstNameMax'))
+      .required(t('validation:firstNameReq')),
+    lastname: Yup.string()
+      .min(2, t('validation:lastNameMin'))
+      .max(20, t('validation:lastNameMax'))
+      .required(t('validation:lastNameReq')),
+    email: Yup.string()
+      .email(t('validation:emailValid'))
+      .required(t('validation:emailValid')),
+    phone: Yup.string()
+      .matches(/^\+?\d+$/, t('validation:phoneMatches'))
+      .min(9, t('validation:phoneMin'))
+      .max(12, t('validation:phoneMax'))
+      .required(t('validation:phoneReq')),
+    numberOfGuests: Yup.number()
+      .min(1, t('validation:guestsReq'))
+      .max(7, t('validation:guestsReq'))
+      .required(t('validation:guestsReq')),
+    customNumberOfGuests: Yup.number()
+      .min(7, t('validation:guestsReq'))
+      .max(90, t('validation:guestsReq')),
+    gdpr: Yup.boolean().oneOf(
+      [true],
+      t('validation:gdprReq')
+    )
+  })
+
   return (
     <Formik
       initialValues={{
-        numberOfGuests: 1,
+        numberOfGuests: 0,
         customNumberOfGuests: 7,
         date: '',
         time: '',
@@ -77,7 +109,7 @@ export const BookingForm = () => {
                   ? values.customNumberOfGuests
                   : values.numberOfGuests) / 6
               ) -
-                1
+              1
             ) {
               // This posts the last booking with the right amount of people
               let peopleAtTheLastTable
@@ -123,119 +155,137 @@ export const BookingForm = () => {
         setSubmitted(true)
       }}
     >
-      {({ values, errors, touched }) => (
-        <>
-          {!submitted ? (
-            <Form>
-              <FieldGroup name={t('guests')}>
-                <RadioButtonContainer
-                  role="group"
-                  aria-labelledby="my-radio-group"
-                >
-                  <RadioButton
-                    label="1"
-                    name="numberOfGuests"
-                    id="1"
-                    value={1}
-                  />
-                  <RadioButton
-                    label="2"
-                    name="numberOfGuests"
-                    id="2"
-                    value={2}
-                  />
-                  <RadioButton
-                    label="3"
-                    name="numberOfGuests"
-                    id="3"
-                    value={3}
-                  />
-                  <RadioButton
-                    label="4"
-                    name="numberOfGuests"
-                    id="4"
-                    value={4}
-                  />
-                  <RadioButton
-                    label="5"
-                    name="numberOfGuests"
-                    id="5"
-                    value={5}
-                  />
-                  <RadioButton
-                    label="6"
-                    name="numberOfGuests"
-                    id="6"
-                    value={6}
-                  />
-                  <RadioButton
-                    label="6+"
-                    name="numberOfGuests"
-                    id="7"
-                    value={7}
-                  />
-                  {errors.numberOfGuests && touched.numberOfGuests && (
-                    <Text
-                      css={{
-                        color: 'red',
-                        position: 'absolute',
-                        bottom: '-1.25rem'
-                      }}
-                      type="small"
-                    >
-                      {errors.numberOfGuests}
-                    </Text>
+      {({ values, errors, touched, setFieldTouched }) => {
+        i18n.on('languageChanged', () => {
+          Object.keys(errors).forEach(fieldName => {
+            setFieldTouched(fieldName)
+          })
+        })
+        return (
+          <>
+            {!submitted ? (
+              <Form>
+                <FieldGroup name={t('guests')}>
+                  <RadioButtonContainer
+                    role="group"
+                    aria-labelledby="my-radio-group"
+                  >
+                    <RadioButton
+                      label="1"
+                      name="numberOfGuests"
+                      id="1"
+                      value={1}
+                    />
+                    <RadioButton
+                      label="2"
+                      name="numberOfGuests"
+                      id="2"
+                      value={2}
+                    />
+                    <RadioButton
+                      label="3"
+                      name="numberOfGuests"
+                      id="3"
+                      value={3}
+                    />
+                    <RadioButton
+                      label="4"
+                      name="numberOfGuests"
+                      id="4"
+                      value={4}
+                    />
+                    <RadioButton
+                      label="5"
+                      name="numberOfGuests"
+                      id="5"
+                      value={5}
+                    />
+                    <RadioButton
+                      label="6"
+                      name="numberOfGuests"
+                      id="6"
+                      value={6}
+                    />
+                    <RadioButton
+                      label="6+"
+                      name="numberOfGuests"
+                      id="7"
+                      value={7}
+                    />
+                    {errors.numberOfGuests && touched.numberOfGuests && (
+                      <Text
+                        css={{
+                          color: 'red',
+                          position: 'absolute',
+                          bottom: '-1.25rem'
+                        }}
+                        type="small"
+                      >
+                        {errors.numberOfGuests}
+                      </Text>
+                    )}
+                  </RadioButtonContainer>
+
+                  {values.numberOfGuests > 6 && (
+                    <Input
+                      type="number"
+                      name="customNumberOfGuests"
+                      id="customNumberOfGuests"
+                      isError={false}
+                      errorMsg="Hej"
+                      max={90}
+                      min={7}
+                    />
                   )}
-                </RadioButtonContainer>
+                </FieldGroup>
 
-                {values.numberOfGuests > 6 && (
-                  <Input
-                    type="number"
-                    name="customNumberOfGuests"
-                    id="customNumberOfGuests"
-                    isError={false}
-                    errorMsg="Hej"
-                    max={90}
-                    min={7}
-                  />
-                )}
-              </FieldGroup>
+                <FieldGroup name={t('date')}>
+                  <CustomDatePicker />
+                </FieldGroup>
 
-              <FieldGroup name={t('date')}>
-                <CustomDatePicker />
-              </FieldGroup>
+                <FieldGroup name={t('contactInformation')}>
+                  <InputContainer>
+                    <Input
+                      label={t('firstName')}
+                      id="name"
+                      name="name"
+                      type="text"
+                      placeholder={t('firstName')}
+                      isError={errors.name && touched.name ? true : false}
+                      errorMsg={errors.name}
+                    />
 
-              <FieldGroup name={t('contactInformation')}>
-                <InputContainer>
-                  <Input
-                    label={t('firstName')}
-                    id="name"
-                    name="name"
-                    type="text"
-                    placeholder={t('firstName')}
-                    isError={errors.name && touched.name ? true : false}
-                    errorMsg={errors.name}
-                  />
+                    <Input
+                      label={t('lastName')}
+                      id="lastname"
+                      name="lastname"
+                      type="text"
+                      placeholder={t('lastName')}
+                      isError={errors.lastname && touched.lastname ? true : false}
+                      errorMsg={errors.lastname}
+                    />
 
-                  <Input
-                    label={t('lastName')}
-                    id="lastname"
-                    name="lastname"
-                    type="text"
-                    placeholder={t('lastName')}
-                    isError={errors.lastname && touched.lastname ? true : false}
-                    errorMsg={errors.lastname}
-                  />
+                    <Input
+                      label="Email"
+                      id="email"
+                      name="email"
+                      type="text"
+                      placeholder="Email"
+                      isError={errors.email && touched.email ? true : false}
+                      errorMsg={errors.email}
+                    />
 
-                  <Input
-                    label="Email"
-                    id="email"
-                    name="email"
-                    type="text"
-                    placeholder="Email"
-                    isError={errors.email && touched.email ? true : false}
-                    errorMsg={errors.email}
-                  />
+                    <Input
+                      label={t('phoneNumber')}
+                      id="phone"
+                      name="phone"
+                      type="text"
+                      placeholder="ex: +4612345678"
+                      isError={errors.phone && touched.phone ? true : false}
+                      errorMsg={errors.phone}
+                    />
+                  </InputContainer>
+                </FieldGroup>
 
                   <Input
                     label={t('phoneNumber')}
@@ -302,21 +352,73 @@ export const BookingForm = () => {
 
               <Link to="/">
                 <Text
-                  css={{
-                    color: '$secondary',
-                    marginTop: '1rem',
-                    marginBottom: '16rem',
-                    display: 'block'
-                  }}
-                  as="span"
+                  css={{ marginTop: '2rem', marginBottom: '0.25rem' }}
+                  as="h2"
+                  type="title4"
                 >
-                  {t('back')}
+                  GDPR
                 </Text>
-              </Link>
-            </div>
-          )}
-        </>
-      )}
+                <RadioButtonContainer>
+                  <Checkbox
+                    label={t('gdpr')}
+                    id="gdpr"
+                    name="gdpr"
+                    value={true}
+                    isError={errors.gdpr && touched.gdpr ? true : false}
+                    errorMsg={errors.gdpr}
+                  />
+                </RadioButtonContainer>
+
+                <Button
+                  type="submit"
+                  size="large"
+                  variant="secondary"
+                  css={{ marginTop: '2rem' }}
+                >
+                  {t('booking')}
+                </Button>
+              </Form>
+            ) : (
+              <div>
+                <Text as="h3" type="title3">
+                  {t('sent')}
+                </Text>
+                <Text>
+                  {t('thanks')} {values.name}!
+                </Text>
+                <Text>
+                  {t('booked')}{' '}
+                  {format(new Date(values.date), 'cccc', {
+                    locale: i18n.language === 'sv' ? sv : enGB
+                  })}{' '}
+                  {t('when')}{' '}
+                  {format(new Date(values.date), 'do LLLL', {
+                    locale: i18n.language === 'sv' ? sv : enGB
+                  })}{' '}
+                  {t('at')} {values.time} {t('for')}{' '}
+                  {values.numberOfGuests > 6
+                    ? values.customNumberOfGuests
+                    : values.numberOfGuests}{' '}
+                  {t('person')}
+                </Text>
+
+                <Link to="/">
+                  <Text
+                    css={{
+                      color: '$secondary',
+                      marginTop: '1rem',
+                      marginBottom: '16rem',
+                      display: 'block'
+                    }}
+                    as="span"
+                  >
+                    {t('back')}
+                  </Text>
+                </Link>
+              </div>
+            )}
+          </>)
+      }}
     </Formik>
   )
 }
--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,3 +1,54 @@
+import { Text } from 'components/primitives'
+import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+import { styled } from 'stitches.config'
+
 export const NotFound = () => {
-  return <div>NotFound</div>
+  const { t } = useTranslation()
+
+  return (
+    <Container>
+      <NotFoundContainer aria-label="404 Page not found">
+        <Four aria-hidden>4</Four>
+        <img aria-hidden src="img/logo.png" alt="" height={96} width={96} />
+        <Four aria-hidden>4</Four>
+      </NotFoundContainer>
+      <Text type="large" css={{ color: '$secondary' }}>
+        {t('notFound')}
+      </Text>
+      <Link to="/">
+        <Text
+          css={{
+            color: '$secondary',
+            marginTop: '3rem',
+            display: 'block'
+          }}
+          as="span"
+        >
+          ← {t('back')}
+        </Text>
+      </Link>
+    </Container>
+  )
 }
+
+const Four = styled('h1', {
+  fontSize: '8rem',
+  fontWeight: '$bold',
+  color: '$secondary'
+})
+
+const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '80vh',
+  width: '100%'
+})
+
+const NotFoundContainer = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+})
